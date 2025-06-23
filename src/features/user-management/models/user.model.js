646047b563f4b4c2d3edc35097a17c1bf573cb04
Sam <samirajetzabel@gmail.com>
2025-06-23 02:02:38 -0500

    feat: add pinia for users

---
+++ src/features/user-management/models/user.model.js
@@ -0,0 +1,15 @@
+export class User {
+    id
+    name
+    username
+    email
+    phone
+
+    constructor(id, name, username, email, phone) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+}
